ea2d721fdaa32c58a32795eb368ff05a3167443c
ichramsyah <iamabdulrahman15@gmail.com>
2026-01-18 17:37:49 +0700

    update sponsor

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Sponsor from './components/Sponsor';
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <Navbar />
       <Hero />
+      <Sponsor />
 
       {/* Placeholder for Services Section */}
       <section id="services" className="min-h-screen flex items-center justify-center text-gray-400">
